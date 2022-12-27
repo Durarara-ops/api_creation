960e25119deabd4b9c29eb4c91f7a7934de14463
Akashy <jen.danilow2013@yandex.ru>
2022-12-27 00:43:00 -0800

    Added translate

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+class Item(BaseModel):
+	text: str
+
+
+app = FastAPI()
+classifier = pipeline("translation_en_to_ru",
+			model = "Helsinki-NLP/opus-mt-en-ru")
+
+
+@app.get("/")
+def root():
+	return{'message': 'Hello word'}
+
+@app.post("/predict/")
+def predict(item: Item):
+	return classifier(item.text)[0]
